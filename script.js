a101c89c38a85a2fb946c73cd2a5cb7d013b94c6
jesse <31576321+agentjesse@users.noreply.github.com>
2024-02-11 18:00:43 -0500

    Improved grid creation logic
    A percentage is set on each squareDiv to use with flex-basis

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 //global state
-const gridSideSquares = 2;
+const gridSideSquares = 6;
 let totalSquares = Math.pow( gridSideSquares, 2 );
 
 // reference nodes
@@ -8,8 +8,13 @@ const container = document.querySelector('.container');
 //create divs in memory
 while (totalSquares) {
   const squareDiv = document.createElement('div');
+  //set individual inline styles without overwriting via CSSStyleDeclaration object
+  //Number's toFixed() rounds to an amount of decimals. must be careful if it rounds up as grid may break.
+  squareDiv.style.flexBasis = `${ (1/gridSideSquares*100).toFixed(3) }%`;
   //classList property returns a DOMTokenList collection for manipulating element's classes with methods
   squareDiv.classList.add('square');
   container.append(squareDiv);
   totalSquares--;
 }
+
+
