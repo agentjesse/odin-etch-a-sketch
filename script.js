7de57f60218ff270516d98686477d895500a2269
jesse <31576321+agentjesse@users.noreply.github.com>
2024-02-11 19:56:44 -0500

    Added some logic to new grid button
    referenced square generation function to be callable again

--- script.js
+++ script.js
@@ -1,27 +1,38 @@
 //global state & node references
-const gridSideSquares = 8;
-let totalSquares = Math.pow( gridSideSquares, 2 );
+const startSideSquares = 8;
+let totalSquares = Math.pow( startSideSquares, 2 );
 const container = document.querySelector('.container');
-const newGridBtn = document.querySelector('gridPrompt');
+const newGridBtn = document.querySelector('.gridPrompt');
 
 //random rgb color function
 const randomRGB = ()=> `rgb(${Math.ceil(Math.random()*255)},${Math.ceil(Math.random()*255)},${Math.ceil(Math.random()*255)})`;
 
-//create and append square divs
-while (totalSquares) {
-  const squareDiv = document.createElement('div');
-  //set individual inline styles without overwriting via CSSStyleDeclaration object
-  //set flexbasis to a percentage with 3 decimal places without rounding up
-  squareDiv.style.flexBasis = `${ Math.floor( 1 / gridSideSquares * 100000 ) / 1000 }%`;
-  //classList property returns a DOMTokenList collection for manipulating element's classes with methods
-  squareDiv.classList.add('square');
-  container.append(squareDiv);
-  totalSquares--;
-}
+//fn to create and append square divs. use default for start
+const generateSquares = (sideSquares = startSideSquares)=> {
+  // console.log(sideSquares);
+  while (totalSquares) {
+    const squareDiv = document.createElement('div');
+    //set individual inline styles without overwriting via CSSStyleDeclaration object
+    //set flexbasis to a percentage with 3 decimal places without rounding up
+    squareDiv.style.flexBasis = `${ Math.floor( 1 / startSideSquares * 100000 ) / 1000 }%`;
+    //classList property returns a DOMTokenList collection for manipulating element's classes with methods
+    squareDiv.classList.add('square');
+    container.append(squareDiv);
+    totalSquares--;
+  }
+};
+generateSquares();
 
 //event listener on parent container to catch bubbling mouseover events of children. no padding on parent, so itwill not affect it.
 container.addEventListener('mouseover', e=> {
   e.stopPropagation();
   //random bg color change when mouse enters
   e.target.style.backgroundColor = randomRGB();
+});
+
+//event listener for button to remove and remake grid
+newGridBtn.addEventListener('click', e=>{
+  e.stopPropagation();
+  totalSquares= Math.pow( 2, 2 );
+  generateSquares(2);
 });
